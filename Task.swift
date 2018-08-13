75e17ee81103cf64e8f05b015441f932371dcd57
Shamar Smith-Standford <shamarss@icloud.com>
2018-08-13 19:49:44 -0400

    task file

---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Shamar Smith-Standford on 8/13/18.
+//  Copyright © 2018 GawdCode. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
